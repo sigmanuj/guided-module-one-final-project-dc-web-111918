30c1c41a160d5134af0bee2ae533b8c1fb6ade6e
github username <github email address>
2018-12-06 18:18:06 -0500

    New seeds

--- bin/run.rb
+++ bin/run.rb
@@ -1,2 +1,7 @@
 require_relative 'config/environment'
 
+def greet
+  puts "Welcome to Movie Source!"
+end
+
+greet
---
+++ db/seeds.rb
@@ -0,0 +1,42 @@
+
+####MOVIES#########
+fast_1 = Movie.create!(title: "The Fast and the Furious")
+fast_2 = Movie.create(title: "2 Fast 2 Furious")
+fast_3 = Movie.create(title: "The Fast and the Furious: Tokyo Drift")
+fast_4 = Movie.create(title: "Fast and Furious")
+fast_5 = Movie.create(title: "Fast Five")
+fast_6 = Movie.create(title: "Fast & Furious 6")
+fast_7 = Movie.create(title: "Furious 7")
+fast_8 = Movie.create!(title: "The Fate of the Furious")
+
+love = Movie.create(title: "Love Actually")
+
+devil = Movie.create!(title: "The Devil Wears Prada")
+
+ps = Movie.create(title: "P.S I Love You")
+
+die_1 = Movie.create(title: "Die Hard")
+
+die_2 = Movie.create(title: "Die Hard 2")
+
+die_3 = Movie.create(title: "Die Hard with a Vengeance")
+
+die_4 = Movie.create(title: "Live Free or Die Hard")
+
+die_5 = Movie.create(title: "A Good Day to Die Hard")
+
+
+
+
+##########MOVIE_SOURCES###############
+
+# movie_source_test = Movie_Source.create(title: "Movie Title", provider: "Source Provider")
+
+ms_1 = Movie_Source.create!(title: fast_1, provider: )
+ms_2 = Movie_Source.create(title: fast_2, provider: )
+ms_3 = Movie_Source.create(title: fast_3, provider: )
+ms_4 = Movie_Source.create(title: fast_4, provider: )
+ms_5 = Movie_Source.create(title: fast_5, provider: )
+ms_6 = Movie_Source.create(title: fast_6, provider: )
+ms_7 = Movie_Source.create(title: fast_7, provider: )
+ms_8 = Movie_Source.create(title: fast_8, provider: )
