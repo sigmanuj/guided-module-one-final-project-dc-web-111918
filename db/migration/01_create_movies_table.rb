d87d11cb2db5bc646be405c384d2c0b223d432ba
github username <github email address>
2018-12-06 18:32:08 -0500

    dem movie sources

--- db/migration/01_create_movies_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateMoviesTable <ActiveRecord::Miration[5.1]
-
-  def change
-    create_table :movies do |t|
-      t.string :title
-    end
-  end
-end
--- db/migration/03_create_source_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateSourceTable <ActiveRecord::Miration[5.1]
-
-  def change
-    create_table :source do |t|
-      t.string :name
-    end
-  end
-end
